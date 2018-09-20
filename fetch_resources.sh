227a0a6bef5eeffae602162ba05514539f4295ac
Toke Eskildsen <toes@kb.dk>
2018-09-20 16:41:34 +0200

    Better feedback, more flexible fetching

--- fetch_resources.sh
+++ fetch_resources.sh
@@ -20,7 +20,11 @@ fi
 : ${SUB_SOURCE:="solr_base"}
 : ${SUB_DEST:="resources"}
 : ${RESOURCE_FIELD:="loar_resource"}
-: ${RESOURCE_EXT:=".xml"}
+: ${RESOURCE_EXT:=""}
+: ${RESOURCE_CHECK_FIELD:=""}
+: ${RESOURCE_CHECK_EXT:=""}
+: ${URL_PREFIX:=""}
+: ${URL_POSTFIX:=""}
 
 usage() {
     echo ""
@@ -49,24 +53,35 @@ fetch_external() {
     pushd $PROJECT > /dev/null
     mkdir -p "$SUB_DEST"
     cd $SUB_SOURCE
+    local TOTAL=$(find . -iname "*.xml" | wc -l)
+    local COUNT=0
     for RECORD in *.xml; do
+        COUNT=$(( COUNT+1 ))
         local DEST="../$SUB_DEST/${RECORD}"
         if [[ -s "$DEST" ]]; then
-            echo "- Already fetched resource for $RECORD"
+            echo "$COUNT/$TOTAL> Already fetched resource for $RECORD"
             continue
+        fi
+        if [[ "." != ".$RESOURCE_CHECK_FIELD" ]]; then
+            local RESOURCE_CHECK=$(grep "<field name=\"$RESOURCE_CHECK_FIELD\">[^<]\+${RESOURCE_CHECK_EXT}</field>" "$RECORD" | sed 's/.*>\([^<]\+\)<.*/\1/')
+            if [[ "." == ".$RESOURCE_CHECK" ]]; then
+                echo "$COUNT/$TOTAL> No $RESOURCE_CHECK_FIELD check field with extension $RESOURCE_CHECK_EXT in $RECORD"
+                continue
+            fi
         fi
         local RESOURCE=$(grep "<field name=\"$RESOURCE_FIELD\">[^<]\+${RESOURCE_EXT}</field>" "$RECORD" | sed 's/.*>\([^<]\+\)<.*/\1/')
         if [[ "." == ".$RESOURCE" ]]; then
-            echo "- No $RESOURCE_FIELD with extension $RESOURCE_EXT in $RECORD"
+            echo "$COUNT/$TOTAL> No $RESOURCE_FIELD field with extension $RESOURCE_EXT in $RECORD"
             continue
         fi
         if [[ $(wc -l <<< "$RESOURCE") -gt 1 ]]; then
-            echo "- Multiple $RESOURCE_FIELD with extension $RESOURCE_EXT in $RECORD - only the first will be fetched"
+            echo "$COUNT/$TOTAL> Multiple $RESOURCE_FIELD with extension $RESOURCE_EXT in $RECORD - only the first will be fetched"
             echo "$RESOURCE"
             RESOURCE=$(head -n 1 <<< "$RESOURCE")
         fi
-        echo "- Fetching resource for $RECORD: $RESOURCE"
-        curl -L -s "$RESOURCE" > "$DEST"
+        local URL="$URL_PREFIX$RESOURCE$URL_POSTFIX"
+        echo "$COUNT/$TOTAL> Fetching resource for $RECORD: $URL"
+        curl -L -s "$URL" > "$DEST"
     done
     popd > /dev/null
 }
