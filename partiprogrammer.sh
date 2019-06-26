e4fd5bab85066e705812dbc4afcf0372c02ad581
Toke Eskildsen <toes@kb.dk>
2019-06-26 16:03:59 +0200

    Adds first full take although not tested properly as the OCR is running slow

--- partiprogrammer.sh
+++ partiprogrammer.sh
@@ -42,21 +42,15 @@ check_parameters() {
 
 check_parameters "$@"
 
-#PROJECT="$PROJECT" COLLECTION="partiprogrammer" SEARCH_URL_PREFIX="$SEARCH_URL_PREFIX" SUB_DEST="kb_meta"  ./download_kb_image_metadata.sh
+PROJECT="$PROJECT" COLLECTION="partiprogrammer" SEARCH_URL_PREFIX="$SEARCH_URL_PREFIX" SUB_DEST="kb_meta"  ./download_kb_image_metadata.sh
 
-#PROJECT="$PROJECT" SUB_SOURCE="kb_meta" SUB_DEST="records" BODY_START_REGEXP="<item.*" ELEMENT_START_REGEXP="<item.*" ELEMENT_END_REGEXP="</item>" SKIP_HEADER="true" SKIP_FOOTER="true" ./split_xml.sh
+PROJECT="$PROJECT" SUB_SOURCE="kb_meta" SUB_DEST="records" BODY_START_REGEXP="<item.*" ELEMENT_START_REGEXP="<item.*" ELEMENT_END_REGEXP="</item>" SKIP_HEADER="true" SKIP_FOOTER="true" ./split_xml.sh
 
-#PROJECT="$PROJECT" XSLT="$(pwd)/kb2solr.xsl" SUB_SOURCE="records" SUB_DEST="solr_base" ./apply_xslt.sh
+PROJECT="$PROJECT" XSLT="$(pwd)/kb2solr.xsl" SUB_SOURCE="records" SUB_DEST="solr_base" ./apply_xslt.sh
 
 PROJECT="$PROJECT" SUB_SOURCE="solr_base" SUB_DEST="pdf_json" RESOURCE_FIELD="external_resource" RESOURCE_EXT=".pdf" EXTERNAL_ANALYZED_RESOURCE_REGEXP="<field[^<]*external_resource.*pdf" URL_PREFIX="$OCR_URL" ALLOW_MULTI="true" RECORD_CALLBACK="$OCR_START" ./fetch_resources.sh
 
-exit
-
-
-PROJECT="$PROJECT" SUB_SOURCE="solr_base" SUB_DEST="pdf_json" RESOURCE_FIELD="external_resource" RESOURCE_EXT=".pdf" RESOURCE_NAME_FIELD="case" RESOURCE_NAME_PRUNE_REGEXP="folkeskole_" EXTERNAL_ANALYZED_RESOURCE_REGEXP="<field[^<]*external_resource.*pdf" URL_PREFIX="$PDF_URL" ALLOW_MULTI="true" ./fetch_resources.sh
-
-
-PROJECT="$PROJECT" SUB_SOURCE="solr_base" SUB_PDF_JSON="pdf_json" SUB_DEST="solr_ready" EXTERNAL_ANALYZED_RESOURCE_REGEXP="<field[^<]*external_resource.*pdf" COLLECTION="folkeskole" ./pdf_enrich.sh
+PROJECT="$PROJECT" SUB_SOURCE="solr_base" SUB_PDF_JSON="pdf_json" SUB_DEST="solr_ready" EXTERNAL_ANALYZED_RESOURCE_REGEXP="<field[^<]*external_resource.*pdf" COLLECTION="partiprogrammer" SKIP_PAGES="1 2" ./pdf_enrich.sh
 
 PROJECT="$PROJECT" ./prepare_solr.sh
 source cloud.conf
